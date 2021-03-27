a1130697502441ed131facbfcdaf38b059ef5c84
francoisswiegers <francois.swiegers@gmail.com>
2021-03-27 10:34:14 +0000

    Created ConfigException

--- redgnu/redgnu-config/src/main/java/com/swieghelm/redgnu/config/ConfigException.java
+++ redgnu/redgnu-config/src/main/java/com/swieghelm/redgnu/config/ConfigException.java
@@ -1,4 +1,12 @@
 package com.swieghelm.redgnu.config;
 
-public class ConfigException {
+public class ConfigException extends RuntimeException {
+
+    public ConfigException(final String message) {
+        super(message);
+    }
+
+    public ConfigException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
 }
